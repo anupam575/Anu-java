feebef61c0fd6797b548ae3fe1da0f76d2e11092
anupam575 <tripathianupam208@gmail.com>
2024-12-30 13:19:08 +0530

    Add files via upload

---
+++ StaticKeyword/Lecture/src/keyword.java
@@ -0,0 +1,29 @@
+
+//int a = 5;
+// car myCar; reference
+//myCar = new Car()= is a object newCar
+// Student student1 = new Student();
+//Student = classname 2. student1 = objectname(reference) 3. new = keyword Student (); 3.(constructor)
+
+
+//Myclass  objRefVar = new MyClass();
+//Myclass= classname 2. objRefVar = Reference variable 3. new = New keyword
+//MyClass = (constructor call);
+// objRefVar is a Reference variable of my class
+// new is a java keyword used to instantiate a class
+//Myclass  objRefVar(Declaration) 2. new is a Insantiation 3. myclass()=
+// initialization
+
+
+// this keyword rfers to the current clas instance variable == current obje
+//this can be used to refer current class instance variable
+// this can be used to invoke current class method (immplicitly)
+//this can be used to invoke current class constructor
+// this can be passeed an argument in the method call
+// this can be passed as argument in the constructor call
+// this can be used to return the current class instance from the method
+
+// method call invokes a method of the current obj current property use current method use
+// static variables = belong to the class not individual instance shared among all instance of the class bank account interest rate percent 
+//static methods can be called without creating an object of the class can only directly access static variables and other static methods
+// No Access to non -static members = staticmethods and blocks cannot directly access non - static member (variables and methods ) of the class
